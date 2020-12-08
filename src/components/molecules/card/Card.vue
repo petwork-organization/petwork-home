c78d09e772a042889961e8aa8abe4ef85da8d361
Jordhan <jordhan.madec@gmail.com>
2020-12-08 20:06:09 +0100

    [Search] Create component

---
+++ src/components/molecules/card/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="rounded-lg bg-white shadow-xl">
+    <div>
+      <slot name="header"></slot>
+    </div>
+
+    <div class="p-4">
+      <slot name="content"></slot>
+    </div>
+
+    <div>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'app-card',
+});
+</script>
---
+++ src/components/organisms/search-card/SearchCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <app-card>
+    <template v-slot:content>
+      <div class="flex">
+        <div class="flex-auto mr-5">
+          <div class="text-bold text-primary-500 mb-1">Quel animal ?</div>
+          <app-input placeholder="Quelle race d'animal ?"></app-input>
+        </div>
+
+        <div class="flex-auto ml-5">
+          <div class="text-bold text-primary-500 mb-1">Quelle région ?</div>
+          <app-input placeholder="Où habitez-vous ?"></app-input>
+        </div>
+      </div>
+
+      <div class="flex justify-center">
+        <app-button prefixIcon="search">Rechercher</app-button>
+      </div>
+    </template>
+  </app-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import AppCard from '@molecules/card/Card.vue';
+import AppButton from '@atoms/button/Button.vue';
+import AppInput from '@atoms/input/Input.vue';
+
+export default defineComponent({
+  components: { AppCard, AppButton, AppInput },
+  name: 'app-search-card',
+});
+</script>
