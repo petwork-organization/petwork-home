6287924e30c72dacd6408a40a5332c8b426575a6
Jordhan <jordhan.madec@gmail.com>
2020-12-16 23:25:39 +0100

    Add wave to header

--- src/components/templates/home/HomeTemplate.vue
+++ src/components/templates/home/HomeTemplate.vue
@@ -2,16 +2,18 @@
   <div class='h-full relative'>
     <the-navbar class='relative z-10'></the-navbar>
 
-    <header class="relative px-4 py-24 md:py-36 sm:px-16 lg:px-32 -mt-20 bg-gradient-to-bl from-secondary-400 to-primary-500">
+    <header class="relative px-4 pb-12 pt-24 md:pt-36 sm:px-16 lg:px-32 -mt-20 bg-gradient-to-b from-secondary-400 to-primary-500">
       <h1 class="w-full lg:w-1/2 font-extrabold text-3xl md:text-5xl text-white">Adopter n'a jamais été aussi simple !</h1>
 
-      <p class='w-full lg:w-1/2 mt-4 sm:mt-12 text-lg text-gray-300'>
+      <p class='w-full lg:w-1/2 mt-4 sm:mt-8 text-lg text-gray-300'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Integer fringilla augue hendrerit est congue pulvinar.
       </p>
     </header>
 
-    <div class="relative flex justify-center -mt-24 z-10">
+    <img src="../../../assets/shapes/wave-primary-500.svg" class="w-full -mt-1 -mb-20 sm:-mb-32 lg:-mb-48 transform rotate-180 relative -z-10">
+
+    <div class="relative flex justify-center z-10">
       <div class="max-w-full mt-8 px-4 w-full sm:max-w-4xl">
         <search-card></search-card>
       </div>
