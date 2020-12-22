42eabae3e8e177a3b2fe53933783f2403137136d
Marais <piemarais@gmail.com>
2020-12-22 22:38:03 +0100

    Add enum SwipeMode on single file

--- src/directives/SwipeDirective.ts
+++ src/directives/SwipeDirective.ts
@@ -1,13 +1,20 @@
 import 'hammerjs';
 
+enum SwipeMode {
+  left = 'swipeleft',
+  right = 'swiperight',
+  up = 'swipeup',
+  down = 'swipedown',
+}
 export class SwipeDirective {
   mounted(el: HTMLElement, binding: any) {
     if (typeof binding.value === 'function') {
       const hammer = new Hammer(el);
-      if (binding.modifiers.left) {
-        hammer.on('swipeleft', binding.value);
-      } else if (binding.modifiers.right) {
-        hammer.on('swiperight', binding.value);
+      if (binding.arg) {
+        hammer.on(
+          SwipeMode[binding.arg as keyof typeof SwipeMode],
+          binding.value,
+        );
       } else {
         hammer.on('swipe', binding.value);
       }
